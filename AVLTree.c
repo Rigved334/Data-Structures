11e8cb4c5d79ba5eaffc273ea4174b636389c928
Rigved334 <brighved221@gmail.com>
2026-08-04 21:57:38 +0530

    add AVL tree

--- AVLTree.c
+++ AVLTree.c
@@ -4,13 +4,37 @@
 
 struct Node{
     int data;
+    int height;
     struct Node *right;
     struct Node *left;
 };
 
-struct BST{
+struct AVLT{
     struct Node *root;
 };
+
+int max(int a, int b){
+    if(a > b){
+        return a;
+    }
+    return b;
+}
+
+int height(struct Node *newNode){
+    if(newNode == NULL){
+        return 0;
+    }
+
+    return newNode->height;
+}
+
+int balanceFactor(struct Node *newNode){
+    if(newNode == NULL){
+        return 0;
+    }
+
+    return height(newNode->left) - height(newNode->right);
+}
 
 struct Node* createNode(int data){
     struct Node *newNode = malloc(sizeof(struct Node));
@@ -22,6 +46,65 @@ struct Node* createNode(int data){
     newNode->data = data;
     newNode->right = NULL;
     newNode->left = NULL;
+    newNode->height = 1;
+
+    return newNode;
+}
+
+struct Node *Rrotation(struct Node *y){
+    struct Node *x = y->left;
+    struct Node *T2 = x->right;
+
+    x->right = y;
+    y->left = T2;
+
+    y->height = 1 + max(height(y->left), height(y->right));
+    x->height = 1 + max(height(x->left), height(x->right));
+
+    return x;
+}
+
+struct Node *Lrotation(struct Node *x){
+    struct Node *y = x->right;
+    struct Node *T2 = y->left;
+
+    y->left = x;
+    x->right = T2;
+
+    x->height = 1 + max(height(x->left), height(x->right));
+    y->height = 1 + max(height(y->left), height(y->right));
+
+    return y;
+}
+
+struct Node *rebalance(struct Node *newNode){
+    if(newNode == NULL){
+        return NULL;
+    }
+
+    int BF = balanceFactor(newNode);
+
+    if(BF < -1){
+        int BF_Rchild = balanceFactor(newNode->right);
+        if(BF_Rchild > 0){
+            newNode->right = Rrotation(newNode->right);
+            return Lrotation(newNode);
+        }
+        else if(BF_Rchild <= 0){
+            return Lrotation(newNode);
+        }
+    }
+
+    else if(BF > 1){
+        int BF_Lchild = balanceFactor(newNode->left);
+        if(BF_Lchild >= 0){
+            return Rrotation(newNode);
+        }
+        else if(BF_Lchild < 0){
+            newNode->left = Lrotation(newNode->left);
+            return Rrotation(newNode);
+        }
+    }
 
     return newNode;
 }
@@ -43,7 +126,8 @@ struct Node* insert(struct Node *newNode, int data){
         newNode->right = insert(newNode->right, data);
     }
 
-    return newNode;
+    newNode->height = max(height(newNode->left), height(newNode->right)) + 1;
+    return rebalance(newNode);
 }
 
 bool search(struct Node *newNode, int data){
@@ -105,18 +189,6 @@ struct Node *findMin(struct Node *newNode){
     return findMin(newNode->left);
 }
 
-struct Node *findMax(struct Node *newNode){
-    if(newNode == NULL){
-        return NULL;
-    }
-
-    if(newNode->right == NULL){
-        return newNode;
-    }
-
-    return findMax(newNode->right);
-}
-
 struct Node *deleteNode(struct Node *newNode, int data){
     if(newNode == NULL){
         return NULL;
@@ -150,10 +222,27 @@ struct Node *deleteNode(struct Node *newNode, int data){
         newNode->right = deleteNode(newNode->right, temp->data);
     }
 
-    return newNode;
+    newNode->height = max(height(newNode->left), height(newNode->right)) + 1;
+    return rebalance(newNode);
+}
+
+void printAVLInfo(struct Node *root){
+    if(root == NULL){
+        return;
+    }
+
+    printAVLInfo(root->left);
+
+    printf("Value: %d, Height: %d, BF: %d\n",
+           root->data,
+           root->height,
+           balanceFactor(root));
+
+    printAVLInfo(root->right);
 }
 
 int main(){
+
     struct Node *root = NULL;
 
     root = insert(root, 10);
@@ -168,21 +257,11 @@ int main(){
     printf("\nPost Order Traversal:\n");
     postorderTraversal(root);
     printf("\n");
-    printf("\nSearch 20: %s\n", search(root, 20) ? "Found" : "Not found");
-    printf("Search 50: %s\n", search(root, 50) ? "Found" : "Not found");
+
+    printf("\nInfo about Tree:\n");
+    printAVLInfo(root);
     printf("\n");
-    printf("Minimum:");
-    struct Node *minNode = findMin(root);
-    if (minNode != NULL){
-        printf(" %d\n", minNode->data);
-    }
-    
-    printf("Maximum:");
-    struct Node *maxNode = findMax(root);
-    if (maxNode != NULL){
-        printf(" %d\n", maxNode->data);
-    }
-    printf("\n");
+
     root = deleteNode(root, 10);
     printf("Search 10: %s\n", search(root, 10) ? "Found" : "Not found");
 
